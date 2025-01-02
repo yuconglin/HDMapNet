df2e2286dc392df66300bf1ce707a0f7dbf5b453
Yucong Lin <astroinstrulin@gmail.com>
2025-01-02 10:46:31 -0500

    Update several files to make things run

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ def write_log(writer, ious, title, counter):
 
 def train(args):
     if not os.path.exists(args.logdir):
-        os.mkdir(args.logdir)
+        os.makedirs(args.logdir, exist_ok=True)
     logging.basicConfig(filename=os.path.join(args.logdir, "results.log"),
                         filemode='w',
                         format='%(asctime)s: %(message)s',
@@ -49,8 +49,10 @@ def train(args):
         'angle_class': args.angle_class,
     }
 
-    train_loader, val_loader = semantic_dataset(args.version, args.dataroot, data_conf, args.bsz, args.nworkers)
-    model = get_model(args.model, data_conf, args.instance_seg, args.embedding_dim, args.direction_pred, args.angle_class)
+    train_loader, val_loader = semantic_dataset(
+        args.version, args.dataroot, data_conf, args.bsz, args.nworkers)
+    model = get_model(args.model, data_conf, args.instance_seg,
+                      args.embedding_dim, args.direction_pred, args.angle_class)
 
     if args.finetune:
         model.load_state_dict(torch.load(args.modelf), strict=False)
@@ -61,12 +63,14 @@ def train(args):
                 param.requires_grad = False
     model.cuda()
 
-    opt = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    opt = torch.optim.Adam(model.parameters(), lr=args.lr,
+                           weight_decay=args.weight_decay)
     sched = StepLR(opt, 10, 0.1)
     writer = SummaryWriter(logdir=args.logdir)
 
     loss_fn = SimpleLoss(args.pos_weight).cuda()
-    embedded_loss_fn = DiscriminativeLoss(args.embedding_dim, args.delta_v, args.delta_d).cuda()
+    embedded_loss_fn = DiscriminativeLoss(
+        args.embedding_dim, args.delta_v, args.delta_d).cuda()
     direction_loss_fn = torch.nn.BCELoss(reduction='none')
 
     model.train()
@@ -86,7 +90,8 @@ def train(args):
             instance_gt = instance_gt.cuda()
             seg_loss = loss_fn(semantic, semantic_gt)
             if args.instance_seg:
-                var_loss, dist_loss, reg_loss = embedded_loss_fn(embedding, instance_gt)
+                var_loss, dist_loss, reg_loss = embedded_loss_fn(
+                    embedding, instance_gt)
             else:
                 var_loss = 0
                 dist_loss = 0
@@ -95,22 +100,28 @@ def train(args):
             if args.direction_pred:
                 direction_gt = direction_gt.cuda()
                 lane_mask = (1 - direction_gt[:, 0]).unsqueeze(1)
-                direction_loss = direction_loss_fn(torch.softmax(direction, 1), direction_gt)
-                direction_loss = (direction_loss * lane_mask).sum() / (lane_mask.sum() * direction_loss.shape[1] + 1e-6)
-                angle_diff = calc_angle_diff(direction, direction_gt, args.angle_class)
+                direction_loss = direction_loss_fn(
+                    torch.softmax(direction, 1), direction_gt)
+                direction_loss = (direction_loss * lane_mask).sum() / \
+                    (lane_mask.sum() * direction_loss.shape[1] + 1e-6)
+                angle_diff = calc_angle_diff(
+                    direction, direction_gt, args.angle_class)
             else:
                 direction_loss = 0
                 angle_diff = 0
 
-            final_loss = seg_loss * args.scale_seg + var_loss * args.scale_var + dist_loss * args.scale_dist + direction_loss * args.scale_direction
+            final_loss = seg_loss * args.scale_seg + var_loss * args.scale_var + \
+                dist_loss * args.scale_dist + direction_loss * args.scale_direction
             final_loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+            torch.nn.utils.clip_grad_norm_(
+                model.parameters(), args.max_grad_norm)
             opt.step()
             counter += 1
             t1 = time()
 
             if counter % 10 == 0:
-                intersects, union = get_batch_iou(onehot_encoding(semantic), semantic_gt)
+                intersects, union = get_batch_iou(
+                    onehot_encoding(semantic), semantic_gt)
                 iou = intersects / (union + 1e-7)
                 logger.info(f"TRAIN[{epoch:>3d}]: [{batchi:>4d}/{last_idx}]    "
                             f"Time: {t1-t0:>7.4f}    "
@@ -123,7 +134,8 @@ def train(args):
                 writer.add_scalar('train/var_loss', var_loss, counter)
                 writer.add_scalar('train/dist_loss', dist_loss, counter)
                 writer.add_scalar('train/reg_loss', reg_loss, counter)
-                writer.add_scalar('train/direction_loss', direction_loss, counter)
+                writer.add_scalar('train/direction_loss',
+                                  direction_loss, counter)
                 writer.add_scalar('train/final_loss', final_loss, counter)
                 writer.add_scalar('train/angle_diff', angle_diff, counter)
 
@@ -147,7 +159,8 @@ if __name__ == '__main__':
 
     # nuScenes config
     parser.add_argument('--dataroot', type=str, default='dataset/nuScenes/')
-    parser.add_argument('--version', type=str, default='v1.0-mini', choices=['v1.0-trainval', 'v1.0-mini'])
+    parser.add_argument('--version', type=str, default='v1.0-mini',
+                        choices=['v1.0-trainval', 'v1.0-mini'])
 
     # model config
     parser.add_argument("--model", type=str, default='HDMapNet_cam')
@@ -168,10 +181,14 @@ if __name__ == '__main__':
     # data config
     parser.add_argument("--thickness", type=int, default=5)
     parser.add_argument("--image_size", nargs=2, type=int, default=[128, 352])
-    parser.add_argument("--xbound", nargs=3, type=float, default=[-30.0, 30.0, 0.15])
-    parser.add_argument("--ybound", nargs=3, type=float, default=[-15.0, 15.0, 0.15])
-    parser.add_argument("--zbound", nargs=3, type=float, default=[-10.0, 10.0, 20.0])
-    parser.add_argument("--dbound", nargs=3, type=float, default=[4.0, 45.0, 1.0])
+    parser.add_argument("--xbound", nargs=3, type=float,
+                        default=[-30.0, 30.0, 0.15])
+    parser.add_argument("--ybound", nargs=3, type=float,
+                        default=[-15.0, 15.0, 0.15])
+    parser.add_argument("--zbound", nargs=3, type=float,
+                        default=[-10.0, 10.0, 20.0])
+    parser.add_argument("--dbound", nargs=3, type=float,
+                        default=[4.0, 45.0, 1.0])
 
     # embedding config
     parser.add_argument('--instance_seg', action='store_true')
--- vis_pred.py
+++ vis_pred.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 from PIL import Image
+import os
 
 import matplotlib.pyplot as plt
 
@@ -20,22 +21,26 @@ def onehot_encoding(logits, dim=1):
     return one_hot
 
 
-def vis_segmentation(model, val_loader):
+def vis_segmentation(model, val_loader, logdir):
+    os.makedirs(f'{logdir}/prediction/segmentation', exist_ok=True)
     model.eval()
     with torch.no_grad():
         for batchi, (imgs, trans, rots, intrins, post_trans, post_rots, lidar_data, lidar_mask, car_trans, yaw_pitch_roll, semantic_gt, instance_gt, direction_gt) in enumerate(val_loader):
 
             semantic, embedding, direction = model(imgs.cuda(), trans.cuda(), rots.cuda(), intrins.cuda(),
-                                                post_trans.cuda(), post_rots.cuda(), lidar_data.cuda(),
-                                                lidar_mask.cuda(), car_trans.cuda(), yaw_pitch_roll.cuda())
+                                                   post_trans.cuda(), post_rots.cuda(), lidar_data.cuda(),
+                                                   lidar_mask.cuda(), car_trans.cuda(), yaw_pitch_roll.cuda())
             semantic = semantic.softmax(1).cpu().numpy()
             semantic[semantic_gt < 0.1] = np.nan
 
             for si in range(semantic.shape[0]):
                 plt.figure(figsize=(4, 2))
-                plt.imshow(semantic[si][1], vmin=0, cmap='Blues', vmax=1, alpha=0.6)
-                plt.imshow(semantic[si][2], vmin=0, cmap='Reds', vmax=1, alpha=0.6)
-                plt.imshow(semantic[si][3], vmin=0, cmap='Greens', vmax=1, alpha=0.6)
+                plt.imshow(semantic[si][1], vmin=0,
+                           cmap='Blues', vmax=1, alpha=0.6)
+                plt.imshow(semantic[si][2], vmin=0,
+                           cmap='Reds', vmax=1, alpha=0.6)
+                plt.imshow(semantic[si][3], vmin=0,
+                           cmap='Greens', vmax=1, alpha=0.6)
 
                 # fig.axes.get_xaxis().set_visible(False)
                 # fig.axes.get_yaxis().set_visible(False)
@@ -43,13 +48,14 @@ def vis_segmentation(model, val_loader):
                 plt.ylim(0, 200)
                 plt.axis('off')
 
-                imname = f'eval{batchi:06}_{si:03}.jpg'
+                imname = f'{logdir}/prediction/segmentation/eval{batchi:06}_{si:03}.jpg'
                 print('saving', imname)
                 plt.savefig(imname)
                 plt.close()
 
 
-def vis_vector(model, val_loader, angle_class):
+def vis_vector(model, val_loader, angle_class, logdir):
+    os.makedirs(f'{logdir}/prediction/vectorization', exist_ok=True)
     model.eval()
     car_img = Image.open('icon/car.png')
 
@@ -61,16 +67,18 @@ def vis_vector(model, val_loader, angle_class):
                                                        lidar_mask.cuda(), car_trans.cuda(), yaw_pitch_roll.cuda())
 
             for si in range(segmentation.shape[0]):
-                coords, _, _ = vectorize(segmentation[si], embedding[si], direction[si], angle_class)
+                coords, _, _ = vectorize(
+                    segmentation[si], embedding[si], direction[si], angle_class)
 
                 for coord in coords:
                     plt.plot(coord[:, 0], coord[:, 1], linewidth=5)
 
                 plt.xlim((0, segmentation.shape[3]))
                 plt.ylim((0, segmentation.shape[2]))
-                plt.imshow(car_img, extent=[segmentation.shape[3]//2-15, segmentation.shape[3]//2+15, segmentation.shape[2]//2-12, segmentation.shape[2]//2+12])
+                plt.imshow(car_img, extent=[segmentation.shape[3]//2-15, segmentation.shape[3] //
+                           2+15, segmentation.shape[2]//2-12, segmentation.shape[2]//2+12])
 
-                img_name = f'eval{batchi:06}_{si:03}.jpg'
+                img_name = f'{logdir}/prediction/vectorization/eval{batchi:06}_{si:03}.jpg'
                 print('saving', img_name)
                 plt.savefig(img_name)
                 plt.close()
@@ -88,12 +96,14 @@ def main(args):
         'angle_class': args.angle_class,
     }
 
-    train_loader, val_loader = semantic_dataset(args.version, args.dataroot, data_conf, args.bsz, args.nworkers)
-    model = get_model(args.model, data_conf, args.instance_seg, args.embedding_dim, args.direction_pred, args.angle_class)
+    train_loader, val_loader = semantic_dataset(
+        args.version, args.dataroot, data_conf, args.bsz, args.nworkers)
+    model = get_model(args.model, data_conf, args.instance_seg,
+                      args.embedding_dim, args.direction_pred, args.angle_class)
     model.load_state_dict(torch.load(args.modelf), strict=False)
     model.cuda()
-    vis_vector(model, val_loader, args.angle_class)
-    # vis_segmentation(model, val_loader)
+    vis_vector(model, val_loader, args.angle_class, args.logdir)
+    # vis_segmentation(model, val_loader, args.logdir)
 
 
 if __name__ == '__main__':
@@ -103,7 +113,8 @@ if __name__ == '__main__':
 
     # nuScenes config
     parser.add_argument('--dataroot', type=str, default='dataset/nuScenes/')
-    parser.add_argument('--version', type=str, default='v1.0-mini', choices=['v1.0-trainval', 'v1.0-mini'])
+    parser.add_argument('--version', type=str, default='v1.0-mini',
+                        choices=['v1.0-trainval', 'v1.0-mini'])
 
     # model config
     parser.add_argument("--model", type=str, default='HDMapNet_cam')
@@ -124,10 +135,14 @@ if __name__ == '__main__':
     # data config
     parser.add_argument("--thickness", type=int, default=5)
     parser.add_argument("--image_size", nargs=2, type=int, default=[128, 352])
-    parser.add_argument("--xbound", nargs=3, type=float, default=[-30.0, 30.0, 0.15])
-    parser.add_argument("--ybound", nargs=3, type=float, default=[-15.0, 15.0, 0.15])
-    parser.add_argument("--zbound", nargs=3, type=float, default=[-10.0, 10.0, 20.0])
-    parser.add_argument("--dbound", nargs=3, type=float, default=[4.0, 45.0, 1.0])
+    parser.add_argument("--xbound", nargs=3, type=float,
+                        default=[-30.0, 30.0, 0.15])
+    parser.add_argument("--ybound", nargs=3, type=float,
+                        default=[-15.0, 15.0, 0.15])
+    parser.add_argument("--zbound", nargs=3, type=float,
+                        default=[-10.0, 10.0, 20.0])
+    parser.add_argument("--dbound", nargs=3, type=float,
+                        default=[4.0, 45.0, 1.0])
 
     # embedding config
     parser.add_argument('--instance_seg', action='store_true')
